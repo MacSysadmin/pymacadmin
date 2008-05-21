ce95da8aba0f51f78fb2c1a819c91ca55573b7aa
adamsc <adamsc@a06d0699-be4d-0410-9b60-b1b7deb0f677>
2008-05-21 18:00:55 +0000

    Checkin of kicker-replacement from my personal repository. At this point it supports SystemConfiguration notifications, NSWorkspace notifications and FSEvents with actions being shell scripts and Python functions or objects. Beyond better help and some improved command-line options (e.g. adding simple events) the next major area will be expanded library support for common admin tasks (e.g. dealing with system/network configuration)
    git-svn-id: https://pymacadmin.googlecode.com/svn/trunk@2 a06d0699-be4d-0410-9b60-b1b7deb0f677
    
    --HG--
    extra : convert_revision : f4bcb1865581b29458ca65f271125aed3787e84c

---
+++ contrib/kicker-replacement/NetworkConfig.py
@@ -0,0 +1,7 @@
+# encoding: utf-8
+
+class NetworkConfig(object):
+    """Handles network related changes for kicker-replacement"""
+    def atalk_change(self, context=None, logger=None, **kwargs):
+        logger.info("Atalk? You poor person, you…")
+        
