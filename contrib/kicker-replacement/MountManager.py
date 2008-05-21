619b6f7b47c6c39d5218bf34aafe16271b720f4a
Chris Adams <chris@improbable.org>
2008-05-21 18:00:55 +0000

    Checkin of kicker-replacement from my personal repository. At this point it supports SystemConfiguration notifications, NSWorkspace notifications and FSEvents with actions being shell scripts and Python functions or objects. Beyond better help and some improved command-line options (e.g. adding simple events) the next major area will be expanded library support for common admin tasks (e.g. dealing with system/network configuration)
    git-svn-id: https://pymacadmin.googlecode.com/svn/trunk@2 a06d0699-be4d-0410-9b60-b1b7deb0f677

---
+++ contrib/kicker-replacement/MountManager.py
@@ -0,0 +1,11 @@
+from kicker_replacement import BaseHandler
+
+class MountManager(BaseHandler):
+    def onNSWorkspaceDidMountNotification_(self, aNotification):
+        path = aNotification.userInfo()['NSDevicePath']
+        self.logger.info("Mount: %s" % path)
+
+    def onNSWorkspaceDidUnmountNotification_(self, aNotification):
+        path = aNotification.userInfo()['NSDevicePath']
+        self.logger.info("Unmount: %s" % path)
+        
