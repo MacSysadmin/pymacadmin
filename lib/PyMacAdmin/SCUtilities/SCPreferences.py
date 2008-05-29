76305957b25c076e6edb909b17bf165c55350349
Chris Adams <chris@improbable.org>
2008-05-29 03:53:48 +0000

    Created a proxy-setenv.py utility which can be eval-ed to set the shell http_proxy/ftp_proxy variables based on the current SystemConfiguration settings Created a set-proxy.py utility which allows you to manipulate the system configuration from the command-line Started the utility library off with PyMacAdmin.SCUtilities.SCPreferences, which simplifies the process of editing SystemConfiguration preferences. Currently supports only the proxy settings, as used by set-proxy.py

---
+++ lib/PyMacAdmin/SCUtilities/SCPreferences.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+SCPreferences.py: Simplified interaction with SystemConfiguration preferences
+
+TODO:
+* Refactor getvalue/setvalue code into generic functions for dealing with things other than proxies
+"""
+
+import sys
+import os
+import unittest
+
+from SystemConfiguration import *
+
+class SCPreferences(object):
+  """Utility class for working with the SystemConfiguration framework"""
+  proxy_protocols = ('HTTP', 'FTP', 'SOCKS') # List of the supported protocols
+  session = None
+  
+  def __init__(self):
+    super(SCPreferences, self).__init__()
+    self.session = SCPreferencesCreate(None, "set-proxy", None)
+    SCPreferencesLock(self.session, True)
+  
+  def save(self):
+    if not self.session:
+      return      
+    if not SCPreferencesCommitChanges(self.session) or not SCPreferencesApplyChanges(self.session) or not SCPreferencesUnlock(self.session):
+      raise RuntimeError("SystemConfiguration rejected our changes. Does this account have admin rights?")
+    
+  def set_proxy(self, enable=True, protocol="HTTP", server="localhost", port=3128):
+    new_settings = SCPreferencesPathGetValue(self.session, u'/NetworkServices/')
+
+    for interface in new_settings:
+      new_settings[interface]['Proxies']["%sEnable" % protocol]  = 1 if enable else 0
+      if enable:
+        new_settings[interface]['Proxies']['%sPort' % protocol]  = port
+        new_settings[interface]['Proxies']['%sProxy' % protocol] = server
+    
+    SCPreferencesPathSetValue(self.session, u'/NetworkServices/', new_settings)
+
+class SCPreferencesTests(unittest.TestCase):
+  def setUp(self):
+    raise RuntimeError("Thwack Chris about not writing these yet")
+
+if __name__ == '__main__':
+  unittest.main()
---
+++ lib/PyMacAdmin/SCUtilities/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+SCUtilities: tools for dealing with Apple's SystemConfiguration framework
+
+Created by Chris Adams on 2008-05-28.
+
+TODO:
+* Import code related to SystemConfiguration events
+"""
+
+import sys
+import os
+import unittest
+
+class SCUtilitiesTests(unittest.TestCase):
+  def setUp(self):
+    raise RuntimeError("Thwack Chris about not writing these yet")
+
+if __name__ == '__main__':
+  unittest.main()
