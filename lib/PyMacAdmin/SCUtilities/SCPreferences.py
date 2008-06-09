b72351c52340d56cda16e33d3e32471ed6dfdf9d
Chris Adams <chris@improbable.org>
2008-06-09 07:28:11 +0000

    Fixed a bug in add_fs_notifications More consistent whitespace

--- lib/PyMacAdmin/SCUtilities/SCPreferences.py
+++ lib/PyMacAdmin/SCUtilities/SCPreferences.py
@@ -14,35 +14,35 @@ import unittest
 from SystemConfiguration import *
 
 class SCPreferences(object):
-  """Utility class for working with the SystemConfiguration framework"""
-  proxy_protocols = ('HTTP', 'FTP', 'SOCKS') # List of the supported protocols
-  session = None
-  
-  def __init__(self):
-    super(SCPreferences, self).__init__()
-    self.session = SCPreferencesCreate(None, "set-proxy", None)
-    SCPreferencesLock(self.session, True)
-  
-  def save(self):
-    if not self.session:
-      return      
-    if not SCPreferencesCommitChanges(self.session) or not SCPreferencesApplyChanges(self.session) or not SCPreferencesUnlock(self.session):
-      raise RuntimeError("SystemConfiguration rejected our changes. Does this account have admin rights?")
+    """Utility class for working with the SystemConfiguration framework"""
+    proxy_protocols = ('HTTP', 'FTP', 'SOCKS') # List of the supported protocols
+    session = None
     
-  def set_proxy(self, enable=True, protocol="HTTP", server="localhost", port=3128):
-    new_settings = SCPreferencesPathGetValue(self.session, u'/NetworkServices/')
+    def __init__(self):
+        super(SCPreferences, self).__init__()
+        self.session = SCPreferencesCreate(None, "set-proxy", None)
+        SCPreferencesLock(self.session, True)
+    
+    def save(self):
+        if not self.session:
+            return            
+        if not SCPreferencesCommitChanges(self.session) or not SCPreferencesApplyChanges(self.session) or not SCPreferencesUnlock(self.session):
+            raise RuntimeError("SystemConfiguration rejected our changes. Does this account have admin rights?")
+        
+    def set_proxy(self, enable=True, protocol="HTTP", server="localhost", port=3128):        
+        new_settings = SCPreferencesPathGetValue(self.session, u'/NetworkServices/')
 
-    for interface in new_settings:
-      new_settings[interface]['Proxies']["%sEnable" % protocol]  = 1 if enable else 0
-      if enable:
-        new_settings[interface]['Proxies']['%sPort' % protocol]  = port
-        new_settings[interface]['Proxies']['%sProxy' % protocol] = server
-    
-    SCPreferencesPathSetValue(self.session, u'/NetworkServices/', new_settings)
+        for interface in new_settings:
+            new_settings[interface]['Proxies']["%sEnable" % protocol] = 1 if enable else 0
+            if enable:
+                new_settings[interface]['Proxies']['%sPort' % protocol]  = port
+                new_settings[interface]['Proxies']['%sProxy' % protocol] = server
+        
+        SCPreferencesPathSetValue(self.session, u'/NetworkServices/', new_settings)
 
 class SCPreferencesTests(unittest.TestCase):
-  def setUp(self):
-    raise RuntimeError("Thwack Chris about not writing these yet")
+    def setUp(self):
+        raise RuntimeError("Thwack Chris about not writing these yet")
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
