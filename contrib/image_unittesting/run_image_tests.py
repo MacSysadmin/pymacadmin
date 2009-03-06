29039d96769d503dca780b0dc51cacdd891e7490
adamsc <adamsc@a06d0699-be4d-0410-9b60-b1b7deb0f677>
2009-03-06 02:22:09 +0000

    Updated run_image_tests.py, added example tests.
    run_image_tests.py:
      * use optparse.
      * Added several more command line options.
      * Tests now must be descended from macdmgtest.DMGUnitTest
    
    macdmgtest.py:
      * Provides convenience methods for divining the true path of paths
        relative to the dmg mount point, whether a given file path exists
        on the dmg.
      * Provides convenience method to get paths to config files needed by
        tests.
    
    tests/*_test.py: Example tests that are generic enough to be useful to
                     people outside google.
    
    Signed-off-by: Chris Adams <chris@improbable.org>
    
    git-svn-id: https://pymacadmin.googlecode.com/svn/trunk@30 a06d0699-be4d-0410-9b60-b1b7deb0f677

--- contrib/image_unittesting/run_image_tests.py
+++ contrib/image_unittesting/run_image_tests.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/python2.5
+#
+# Use 2.5 so we can import objc & Foundation in tests
 #
 # Copyright 2008 Google Inc.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -14,7 +16,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 """Parent script for unit testing a Mac OS X image candidate.
 
 This script expects to be passed the path to a dmg that is a Mac OS X
@@ -23,13 +24,18 @@ builds up a test suite of their test functions and runs them on the image.
 
 Each test has "self.mountpoint" set that is the mountpoint of the dmg
 
+Unit tests in the test directory must be subclasses of macdmgtest.DMGUnitTest.
+
 Naming formats must be as follows:
+  Files:    *_test.py
   Classes:  Test*
   Tests:    test*
 
-Original Author: Nigel Kersten (nigelk@google.com)
+Author: Nigel Kersten (nigelk@google.com)
+Modified by: Joe Block (jpb@google.com)
 """
 
+import optparse
 import os
 import re
 import subprocess
@@ -39,14 +45,12 @@ import unittest
 import plistlib
 
 
-tests_dir = "tests"
-
-
 def AttachDiskImage(path):
   """attaches a dmg, returns mountpoint, assuming only one filesystem."""
-  
-  command = ["hdiutil", "attach", path, "-mountrandom", "/tmp",
-             "-readonly", "-nobrowse", "-noautoopen", "-plist"]
+
+  command = ["/usr/bin/hdiutil", "attach", path, "-mountrandom", "/tmp",
+             "-readonly", "-nobrowse", "-noautoopen", "-plist",
+             "-owners", "on"]
   task = subprocess.Popen(command,
                           stdout=subprocess.PIPE,
                           stderr=subprocess.PIPE)
@@ -68,14 +72,13 @@ def AttachDiskImage(path):
 def DetachDiskImage(path):
   """forcibly unmounts a given dmg from the mountpoint path."""
   
-  command = ["hdiutil", "detach", path]
+  command = ["/usr/bin/hdiutil", "detach", path]
   returncode = subprocess.call(command)
   if returncode:
-    command = ["hdiutil", "detach", "-force", path]
+    command = ["/usr/bin/hdiutil", "detach", "-force", path]
     returncode = subprocess.call(command)
     if returncode:
       raise StandardError("Unable to unmount dmg mounted at: %s" % path)
-      
   return True
 
 
@@ -91,26 +94,24 @@ def TestClasses(module):
   return classes
 
 
-def RunTest(path, mountpoint):
-  """run_test from path to module, returns testresult."""
-  verbosity = 2
+def GetTestSuite(path, mountpoint, options):
+  """Given path to module, returns suite of test methods."""
   dirname = os.path.dirname(path)
   filename = os.path.basename(path)
-  
+
   if not dirname in sys.path:
     sys.path.append(dirname)
-  
+
   modulename = re.sub("\.py$", "", filename)
   module = __import__(modulename)
   for classname in TestClasses(module):
     test_loader = unittest.TestLoader()
-    test_result = unittest.TestResult()
     test_suite = test_loader.loadTestsFromName(classname, module)
     # there must be a better way than this protected member access...
     for test in test_suite._tests:
-      test.mountpoint = mountpoint
-    test_result = unittest.TextTestRunner(verbosity=verbosity).run(test_suite)
-    return test_result
+      test.SetMountpoint(mountpoint)
+      test.SetOptions(options)
+    return test_suite
 
 
 def ListTests(path):
@@ -121,37 +122,86 @@ def ListTests(path):
   for test in os.listdir(path):
     if pattern.match(test):
       tests.append(test)
+  tests.sort()
   return tests
+
+
+def SummarizeResults(result):
+  """Print a summary of a test result."""
+  print
+  print "Results"
+  print "==============="
+
+  print "total tests run: %s" % result.testsRun
+  print "   errors found: %s" % len(result.errors)
+  print " failures found: %s" % len(result.failures)
+  print
+
+
+def ParseCLIArgs():
+  """Parse command line arguments and set options accordingly."""
+  cli = optparse.OptionParser()
+  cli.add_option("-c", "--configdir", dest="configdir", default=".",
+                 type="string", help="specify directory for test config files")
+  cli.add_option("-d", "--dmg", dest="dmg", type="string",
+                 help="specify path to dmg to test.")
+  cli.add_option("-p", "--pkgdir", dest="pkgdir", type="string",
+                 help="specify directory to look for packages in.")
+  cli.add_option("-r", "--root", dest="root", type="string",
+                 help="specify path to root of a directory tree to test.")
+  cli.add_option("-t", "--testdir", dest="testdir", default="tests",
+                 type="string", help="specify directory with tests")
+  cli.add_option("-v", "--verbosity", type="int", dest="verbosity",
+                 help="specify verbosity level", default=0)
+  (options, args) = cli.parse_args()
+  return (options, args)
 
 
 def main():
   """entry point."""
-  
-  if not len(sys.argv) == 2:
-    print "You must pass the path to a dmg as the only argument"
-    sys.exit(2)
-  
-  dmg = sys.argv[1]
-  
-  print "Mounting disk image... (this may take some time)"
-  mountpoint = AttachDiskImage(dmg)
-  
-  if not mountpoint:
-    print "Unable to mount %s" % dmg
-    sys.exit(2)
-  
+  (options, unused_args) = ParseCLIArgs()
+  dmg = options.dmg
+  verbosity = options.verbosity
+  tests_dir = options.testdir
+  config_dir = options.configdir
+  root = options.root
+  if not dmg and not root:
+    print "Use --dmg to specify a dmg file or --root to specify a directory."
+    sys.exit(1)
+  if dmg:
+    print "Mounting disk image... (this may take some time)"
+    mountpoint = AttachDiskImage(dmg)
+    if not mountpoint:
+      print "Unable to mount %s" % dmg
+      sys.exit(2)
+  elif root:
+    if not os.path.isdir(root):
+      print "%s not a directory" % root
+      sys.exit(2)
+    mountpoint = root
+    print "Checking %s" % mountpoint
+  print
+
   dirname = os.path.dirname(sys.argv[0])
   os.chdir(dirname)
   tests = ListTests(tests_dir)
   test_results = {}
+  combo_suite = unittest.TestSuite()
   for test in tests:
     test_path = os.path.join(tests_dir, test)
-    test_results[test] = RunTest(test_path, mountpoint)
-  
-  # not doing anything with the test results yet.
-  # not sure if it's worth it given the nice existing output...?
-  
-  DetachDiskImage(mountpoint)
+    combo_suite.addTests(GetTestSuite(test_path, mountpoint, options))
+
+  test_results = unittest.TextTestRunner(verbosity=verbosity).run(combo_suite)
+
+  if dmg:
+    DetachDiskImage(mountpoint)
+
+  if test_results.wasSuccessful():
+    sys.exit(0)
+  else:
+    SummarizeResults(test_results)
+    bad = len(test_results.errors) + len(test_results.failures)
+    sys.exit(bad)
 
 
 if __name__ == "__main__":
---
+++ contrib/image_unittesting/tests/zz_world_writable_test.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python2.4
+#
+# Copyright 2008 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Tests are done in alphabetical order by file name, so since this
+# traverses the entire dmg, using zz to push it to the end with the
+# other slow unit tests.
+
+"""Confirm all world-writable files and dirs on the dmg are in our whitelist."""
+__author__ = 'jpb@google.com (Joe Block)'
+
+
+import logging
+import dmgtestutilities
+import macdmgtest
+
+
+def CatalogWritables(path):
+  """Finds all the files and directories that are world-writeable.
+
+  Args:
+    path: root of directory tree to examine.
+  Returns:
+    dictionary of paths to world-writeable files & directories under root,
+    with the base path to the root peeled off.
+  """
+
+  dir_cmd = ['/usr/bin/find', path, '-type', 'd', '-perm', '+o=w', '-exec',
+             'ls', '-ld', '{}', ';']
+  file_cmd = ['/usr/bin/find', path, '-type', 'f', '-perm', '+o=w', '-exec',
+              'ls', '-l', '{}', ';']
+  logging.debug('Searching dmg for world writable files')
+  files = dmgtestutilities.ProcessCommand(file_cmd)
+  logging.debug('Searching dmg for world writable directories')
+  dirs = dmgtestutilities.ProcessCommand(dir_cmd)
+  state_of_sin = dirs['stdout'] + files['stdout']
+
+  writeables = []
+  prefix_length = len(path)
+  for s in state_of_sin:
+    if s:
+      snip = s.find('/')
+      if snip:
+        snip_index = snip + prefix_length
+        rawpath = s[snip_index:]
+        writeables.append(rawpath)
+      else:
+        logging.warn('snip: %s' % snip)
+        logging.warn(s)
+  return writeables
+
+
+def ReadWhiteList(path):
+  """Read a whitelist of world writable files and directories into a dict.
+
+  Ignore lines starting with a # so we can comment the whitelist.
+
+  Args:
+    path: file to load the whitelist from.
+  Returns:
+    dictionary of path:True mappings
+  """
+  white_file = open(path, 'r')
+  catalog = {}
+  for entry in white_file:
+    if not entry or entry[:1] == '#':
+      pass   # ignore comment and empty lines
+    else:
+      catalog[entry.strip()] = True
+  return catalog
+
+
+class TestWritableDirectoriesAndFiles(macdmgtest.DMGUnitTest):
+
+  def setUp(self):
+    whitelist_path = self.ConfigPath('writables.whitelist')
+    self.whitelisted_writables = ReadWhiteList(whitelist_path)
+
+  def testForWorldWritableFilesOrDirectories(self):
+    """SLOW: Search for non-whitelisted world-writable files and directories."""
+    scrutinize = CatalogWritables(self.Mountpoint())
+    sinners = []
+    for s in scrutinize:
+      if s not in self.whitelisted_writables:
+        sinners.append(s)
+    if sinners:
+      print '\n\n# world-writable files & dirs for pasting into whitelist.'
+      print '\n'.join(sinners)
+      print '# end paste\n\n'
+    self.assertEqual(0, len(sinners))
+
+
+if __name__ == '__main__':
+  macdmgtest.main()
