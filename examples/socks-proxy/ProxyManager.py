b7f552b8dfd4ad3ecd788e4407616d624d5737ff
Chris Adams <chris@improbable.org>
2009-03-06 23:55:34 -0500

    Fixed a bug in SCPreferences which caused the proxy port to be written out as a string rather than an integer. This breaks System Preferences Added an example for selectively enabling or disabling a local SOCKS proxy
    --HG--
    extra : convert_revision : 7e14af449062880d0f91ad2aa0ac2e5a5bfed3de

---
+++ examples/socks-proxy/ProxyManager.py
@@ -0,0 +1,26 @@
+from kicker_replacement import BaseHandler
+import socket
+from PyMacAdmin.SCUtilities.SCPreferences import SCPreferences
+
+class ProxyManager(BaseHandler):
+    def __init__(self):
+        self.sc_prefs     = SCPreferences()
+        self.socks_server = 'localhost'
+        self.socks_port   = '1080'
+
+    def onNSWorkspaceDidMountNotification_(self, aNotification):        
+        """A simple method which exists so this can be used for the workspace mount notification during testing"""
+        return self.network_changed()
+        
+    def network_changed(self):
+        # We want to enable the server when our hostname is not on the corporate network:
+        current_address = socket.gethostbyname(socket.getfqdn())
+        new_state       = not current_address.startswith('198.207.70')
+        self.logger.info("Current address is now %s: SOCKS proxy will be %s" % (current_address, "Enabled" if new_state else "Disabled"))
+        try:
+            self.sc_prefs.set_proxy(enable=new_state, protocol='SOCKS', server=self.socks_server, port=self.socks_port)
+            self.sc_prefs.save()
+            self.logger.info("Successfully updated SOCKS proxy setting")
+        except RuntimeError, e:
+          self.logger.error("Unable to set SOCKS proxy setting: %s" % e.message)
+        
