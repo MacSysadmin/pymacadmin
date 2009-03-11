be6c6415f58e7fd8494307675ef239175ebc73af
adamsc <adamsc@a06d0699-be4d-0410-9b60-b1b7deb0f677>
2009-03-11 01:09:38 +0000

    Squashed commit of the following:
    commit 866bfa58161c2d0023d6f9e30cbcb32ac4f854b3
    Author: Chris Adams <chris@improbable.org>
    Date:   Sat Mar 7 15:02:16 2009 -0500
    
        Minor cleanup
    
        Cleaned imports, whitespace, line-length, comments
    
    commit da1fe8ef4eb4945a5d5d689193138eec2cd0705e
    Author: Chris Adams <chris@improbable.org>
    Date:   Sat Mar 7 14:58:30 2009 -0500
    
        Cleanup
    
        Reorganized contrib/ into examples/
        Copied the original keychain-delete which calls ctypes directly from bin
        to examples/ctypes/ so it can be replaced with a wrapper-based version
    
    git-svn-id: https://pymacadmin.googlecode.com/svn/trunk@33 a06d0699-be4d-0410-9b60-b1b7deb0f677
    
    --HG--
    extra : convert_revision : 8be9d820765e7fc277a369c6cd38af8dffa6da4c

---
+++ examples/ctypes/keychain-delete.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Demonstrates how to delete a Keychain item using Python's ctypes library
+"""
+
+import ctypes
+
+service_name     = 'Service Name'
+account_name     = 'Account Name'
+password_length  = ctypes.c_uint32(256)
+password_pointer = ctypes.c_char_p()
+item             = ctypes.c_char_p()
+
+print "Loading Security.framework"
+Security = ctypes.cdll.LoadLibrary('/System/Library/Frameworks/Security.framework/Versions/Current/Security')
+
+print "Searching for the password"
+
+rc = Security.SecKeychainFindGenericPassword(
+    None,
+    len(service_name),
+    service_name,
+    len(account_name),
+    account_name,
+    # Used if you want to  retrieve the password:
+    None, # ctypes.byref(password_length),
+    None, # ctypes.pointer(password_pointer),
+    ctypes.pointer(item)
+)
+
+if rc != 0:
+    raise RuntimeError('SecKeychainFindGenericPassword failed: rc=%d' % rc)
+
+print "Deleting Keychain item"
+
+rc = Security.SecKeychainItemDelete( item )
+
+if rc != 0:
+    raise RuntimeError('SecKeychainItemDelete failed: rc=%d' % rc)
+    
+    
