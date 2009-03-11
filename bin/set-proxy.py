8be9d820765e7fc277a369c6cd38af8dffa6da4c
adamsc <adamsc@a06d0699-be4d-0410-9b60-b1b7deb0f677>
2009-03-11 01:09:38 +0000

    Squashed commit of the following:
    commit 866bfa58161c2d0023d6f9e30cbcb32ac4f854b3
    Author: Chris Adams <chris@improbable.org>
    Date:   Sat Mar 7 15:02:16 2009 -0500
    
        Minor cleanup
    
        Cleaned imports, whitespace, line-length, comments
    
    commit da1fe8ef4eb4945a5d5d689193138eec2cd0705e
    Author: Chris Adams <chris@improbable.org>
    Date:   Sat Mar 7 14:58:30 2009 -0500
    
        Cleanup
    
        Reorganized contrib/ into examples/
        Copied the original keychain-delete which calls ctypes directly from bin
        to examples/ctypes/ so it can be replaced with a wrapper-based version
    
    git-svn-id: https://pymacadmin.googlecode.com/svn/trunk@33 a06d0699-be4d-0410-9b60-b1b7deb0f677

--- bin/set-proxy.py
+++ bin/set-proxy.py
@@ -3,42 +3,56 @@
 """
 %prog --enable --protocol=HTTP --server=proxy.example.edu --port=3128
 
-Configures the system proxy settings from the command-line using the pyMacAdmin PMASystemConfiguration module
+Configures the system proxy settings from the command-line using the
+PyMacAdmin SystemConfiguration module
 """
 
+from PyMacAdmin.SCUtilities.SCPreferences import SCPreferences
 from socket import gethostbyname, gaierror
 import sys
-from PyMacAdmin.SCUtilities.SCPreferences import SCPreferences
+
 
 def main():
     sc_prefs = SCPreferences()
-    
+
     from optparse import OptionParser
     parser = OptionParser(__doc__.strip())
-    parser.add_option('--enable',   dest='enable', action="store_true",               help='Enable proxy for the specified protocol', default=True)
-    parser.add_option('--disable',  dest='enable', action='store_false',              help='Disable proxy for the specified protocol')
-    parser.add_option('--protocol', choices=sc_prefs.proxy_protocols, metavar='PROTOCOL',   help='Specify the protocol (%s)' % ", ".join(sc_prefs.proxy_protocols))
-    parser.add_option('--server',   metavar='SERVER',                                 help="Specify the proxy server's hostname")
-    parser.add_option('--port',     type='int', metavar='PORT',                       help="Specify the proxy server's port")
+
+    parser.add_option('--enable',   dest='enable', action="store_true", default=True,
+        help='Enable proxy for the specified protocol'
+    )
+    parser.add_option('--disable',  dest='enable', action='store_false',
+        help='Disable proxy for the specified protocol'
+    )
+    parser.add_option('--protocol', choices=sc_prefs.proxy_protocols, metavar='PROTOCOL',
+        help='Specify the protocol (%s)' % ", ".join(sc_prefs.proxy_protocols)
+    )
+    parser.add_option('--server',   metavar='SERVER',
+        help="Specify the proxy server's hostname"
+    )
+    parser.add_option('--port',     type='int', metavar='PORT',
+        help="Specify the proxy server's port"
+    )
+
     (options, args) = parser.parse_args()
-    
+
     # optparser inexplicably lacks a require option due to extreme
     # pedanticism but it's not worth switching to argparse:
     if not options.protocol:
         print >> sys.stderr, "ERROR: You must specify a protocol to %s" % ("enable" if options.enable else "disable")
         sys.exit(1)
-    
+
     if options.enable and not ( options.server and options.port ):
             print >> sys.stderr, "ERROR: You must specify a %s proxy server and port" % options.protocol
             sys.exit(1)
-  
+
     if options.server:
         try:
             gethostbyname(options.server)
         except gaierror, exc:
             print >> sys.stderr, "ERROR: couldn't resolve server hostname %s: %s" % (options.server, exc.args[1]) # e.message is broken in the standard socket.gaierror!
             sys.exit(1)
-    
+
     try:
         sc_prefs.set_proxy(enable=options.enable, protocol=options.protocol, server=options.server, port=options.port)
         sc_prefs.save()
