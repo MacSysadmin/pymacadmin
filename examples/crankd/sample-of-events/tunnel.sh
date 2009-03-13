a0cf487e68faae2903913835b8d1b33120249e92
Chris Adams <chris@improbable.org>
2009-03-13 19:29:29 -0400

    Clinton Blackmore's sample-of-events demo
    An example which attempts to monitor every event supported on the
    current system - see this mailing list thread:
    
    http://groups.google.com/group/pymacadmin/browse_thread/thread/15060f66cab0b42d

---
+++ examples/crankd/sample-of-events/tunnel.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+if [[ "$#" -ne "2" ]] ; then cat <<EndUsageText
+    This is a stub/forwarder script.
+    It exists to make it easy to change the target action of a crankd script
+    (ex. to change from 'echo' to 'say', you just have to edit it here)
+    or to send the output to multiple programs.
+
+    It expects two parameters -- an event category, and a specific event
+    For example, the category might be "NSWorkspace" and the event might be
+    NSWorkspaceDidMountNotification.
+EndUsageText
+else
+    CATEGORY=$1
+    SPECIFIC=$2
+    
+    
+    # echoing is only useful if you invoke crankd in a Terminal
+    echo "[$CATEGORY] $SPECIFIC event occurred."
+    
+    # You will need to install growl and growlnotify to use this.
+    # http://growl.info/
+    growlnotify --message "$SPECIFIC event occurred." --title "$CATEGORY event" --name "crankd notification"
+    
+    # Open up Console to see the syslogged events
+    logger "[$CATEGORY] $SPECIFIC event occurred."
+    
+    # Speak the event out loud
+    say "$CATEGORY event occurred."
+
+fi
+
+
+
