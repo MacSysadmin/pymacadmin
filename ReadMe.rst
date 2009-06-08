c14496bf1cd22b7402e285eff535a1a112829901
Chris Adams <chris@improbable.org>
2009-06-08 00:03:03 -0400

    Support for building OS X pkg files using bdist_mpkg
    --HG--
    extra : convert_revision : ccd558935eb923c750a53a9335529f6a17f65dcf

---
+++ ReadMe.rst
@@ -0,0 +1,20 @@
+A collection of Python utilities for Mac OS X system administration.
+
+The PyMacAdmin project started as a collaboration between Chris Adams and
+Nigel Kersten to develop a replacement for the unsupported 'kicker' feature
+included in OS X prior to 10.5. That replacement eventually became crankd,
+which provides a way to execute Python code or a shell script in response to
+many system events: network changes, filesystem activity, application
+launching, etc.
+
+A second major features is the disk image unit testing framework designed to
+test OS X installation images before deployment.
+
+We've also added utilities to create network locations, manage proxy settings
+or update AirPort passwords using our Keychain wrapper.
+
+You're invited join the mailing list:
+
+http://groups.google.com/group/pymacadmin
+
+Please see http://pymacadmin.googlecode.com/ for more information.
--- setup.py
+++ setup.py
@@ -5,7 +5,18 @@ import glob
 import os
 from distutils.command.install import INSTALL_SCHEMES
 
-# Force
+from bdist_mpkg.cmd_bdist_mpkg import bdist_mpkg as bdist_mpkg
+
+class pma_bdist_mpkg(bdist_mpkg):
+    def initialize_options(self):
+        bdist_mpkg.initialize_options(self)
+        self.readme = "ReadMe.html"
+
+    def finalize_options(self):
+        bdist_mpkg.finalize_options(self)
+        self.scheme_map['scripts'] = '/usr/local/sbin'
+
+# For non-pkg installs, tarballs, etc.    
 INSTALL_SCHEMES['unix_prefix']['scripts'] = '$base/sbin'
 
 setup(
@@ -35,5 +46,8 @@ setup(
     packages     = [ 
         ".".join(dirpath.split("/")[1:]) for dirpath, dirnames, filenames in os.walk('lib') if "__init__.py" in filenames
     ],
-    scripts      = glob.glob(os.path.join(os.path.dirname(__file__), 'bin', '*.py'))
+    scripts      = glob.glob(os.path.join(os.path.dirname(__file__), 'bin', '*.py')),
+    cmdclass = {
+        'bdist_mpkg': pma_bdist_mpkg
+    }
 )
